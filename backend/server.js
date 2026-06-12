1a21698b5ee94be86a805f1757fb1b2b21173fb6
Aditya Sharma <adityasharmavats2811@gmail.com>
2026-06-12 18:21:35 +0530

    socketio setup

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 import dotenv from "dotenv";
 dotenv.config();
-
+import http from "http"
 import app from "./src/app.js";
 import connectToDb from "./src/config/database.js";
+import { initSocket } from "./src/sockets/server.socket.js";
+
+const httpServer = http.createServer(app);
+
+initSocket(httpServer)
 
 connectToDb();
 
-app.listen(3000, () => {
+httpServer.listen(3000, () => {
   console.log("Server is running");
 });
 
